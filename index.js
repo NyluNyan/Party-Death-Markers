6e37ec23b6b2678f42d62eee1ef4b787c6b314f7
NyluNyan <39504014+NyluNyan@users.noreply.github.com>
2020-05-15 04:42:50 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,131 +1,41 @@
-module.exports = function PartyDeathMarks(mod)
-{
-	let Members = [],
-	Marks = [],
-	index = 0,
-	markId = 777777777n;
-	//ClassMarker = {1: 91177, 2: 88704, 3: 88704, 4: 88704, 5: 88704, 6: 91113, 7: 91113, 8: 88704, 9: 88704, 10: 91177, 11: 88704, 12: 88704, 13: 88704};
+module.exports = function UnicastFix(mod) {
 	
-	mod.command.add('pd', (cmd, arg1, arg2)=> {
-		if(!isNaN(cmd))
-		{
-			mod.command.message('spawning item');
-			mod.toClient('S_SPAWN_DROPITEM', 8,
-			{
-				gameId: markId,
-				loc: mod.game.info.loc,
-				item: cmd,
-				amount: 1,
-				expiry: 999999,
-				explode: false,
-				masterwork: false,
-				enchant: 0,
-				source: 0n,
-				debug: false,
-				owners: [0n]
-			});
-			markId++;
-		}
+	const brooches = [999001044, 999001045];
+	
+	let users = {},
+	timer = null;
+	
+	mod.command.add('uf', (cmd)=> {
+		mod.log(users);
     });
 	
-	mod.game.on('enter_loading_screen', UnmarkAll);
+	mod.game.on('leave_loading_screen', () => {
+		if(!timer && Object.keys(users).length) timer = mod.setInterval(Clear, 5000);
+    });
 	
-	mod.hook('S_PARTY_MEMBER_LIST', 7, event =>
-	{
-		Members = [];
-		for(var i = 0; i < event.members.length; i++) Members.push(event.members[i].gameId, event.members[i].playerId, event.members[i].class);
-	});
+	mod.hook('S_ABNORMALITY_BEGIN', 4, (event) => {
+		if(brooches.includes(event.id)) users[event.target] = Date.now();
+    });
 	
-	mod.hook('S_SPAWN_DROPITEM', 8, event =>
-	{
-		mod.log(event);
-	});
+	mod.hook('S_SPAWN_USER', 15, (event) => {
+		if(users[event.gameId] && Date.now()-20000 > users[event.gameId]) delete users[event.gameId];
+    });
 	
-	mod.hook('S_SPAWN_USER', 15, event =>
-	{
-		if(!event.alive)
-		{
-			index = Members.indexOf(event.gameId);
-			if(index !== -1 && event.playerId !== mod.game.me.playerId && !Marks.includes(event.playerId)) Mark(event.gameId, event.loc);
-		}
-		else if(Marks.includes(event.gameId))
-		{
-			index = Marks.indexOf(event.gameId);
-			Unmark(Marks[index+2]);
+	mod.hook('S_UNICAST_TRANSFORM_DATA', 6, (event) => {
+		if(users[event.gameId]) {
+			if(Date.now()-1000 > users[event.gameId]) delete users[event.gameId];
+			return false;
 		}
 	});
 	
-	mod.hook('S_CREATURE_LIFE', 3, event =>
-	{
-		if(mod.game.me.gameId === event.gameId) return;
-		if(!event.alive && !Marks.includes(event.gameId))
-		{
-			index = Members.indexOf(event.gameId);
-			Mark(event.gameId, event.loc);
+	function Clear() {
+		var date = Date.now();
+		for(var user in users) {
+			if(date-users[user] > 20000) delete users[user];
 		}
-		else if(event.alive && Marks.includes(event.gameId))
-		{
-			index = Marks.indexOf(event.gameId);
-			Unmark(Marks[index+2]);
+		if(Object.keys(users).length) {
+			mod.clearInterval(timer);
+			timer = null;
 		}
-	});
-	
-	mod.hook('S_LEAVE_PARTY_MEMBER', 2, event =>
-	{
-		if(Marks.includes(event.playerId))
-		{
-			index = Marks.indexOf(event.playerId)-1;
-			Unmark(Marks[index+2]);
-		}
-	});
-	
-	mod.hook('S_LEAVE_PARTY', 1, UnmarkAll);
-	
-	function Mark(gameId, loc)
-	{
-		loc.z -= 100;
-		mod.toClient('S_SPAWN_DROPITEM', 8,
-		{
-			gameId: markId,
-			loc: loc,
-			item: 88704,
-			amount: 1,
-			expiry: 999999,
-			explode: false,
-			masterwork: false,
-			enchant: 0,
-			source: 0n,
-			debug: false,
-			owners: [0n]
-		});
-		Marks.push(gameId, Members[index+1], markId);
-		markId++;
 	}
-	
-	function Unmark(id)
-	{
-		mod.toClient('S_DESPAWN_DROPITEM', 4, { gameId: id });
-		Marks.splice(Marks.indexOf(id)-2, 3);
-	}
-	
-	function UnmarkAll()
-	{
-		if(Marks.length) for(var i = 0; Marks.length > 0; i++) Unmark(Marks[2]);
-		markId = 777777777n;
-	}
-	
-	/*function getMarker(classid)
-	{
-		switch (classid)
-		{
-			case 1:
-			case 10:
-			return 91177;
-			case 6:
-			case 7:
-			return 91113;
-			default:
-			return 88704;
-		}
-	}*/
-}
+};
